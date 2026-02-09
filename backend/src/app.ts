691f0f83fc34481f22c495b567a353a6a44caa2e
Nikhil Sam Mani <nikhilsm1996@gmail.com>
2026-02-10 00:39:50 +0530

    refactor

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { AppDataSource } from "./data-source";
 import projectRoutes from "./routes/projectRoutes";
 import expenseRoutes from "./routes/expenseRoutes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 AppDataSource.initialize()
   .then(() => {
